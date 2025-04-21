a22ff50d376a2c5cb6c69c1947779fdaddece711
Samuel <samdeveloperbr@gmail.com>
2025-04-21 02:58:52 -0300

    Query de criação do banco de dados, ProductDAO e ProductController criados

--- app/src/main/java/com/snsolutions/jackout/MainActivity.java
+++ app/src/main/java/com/snsolutions/jackout/MainActivity.java
@@ -2,11 +2,16 @@ package com.snsolutions.jackout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+
+import com.snsolutions.jackout.controller.ProductController;
+import com.snsolutions.jackout.helper.ConnectionSQL;
+import com.snsolutions.jackout.model.Product;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +22,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        ConnectionSQL connectionSQL  = ConnectionSQL.getInstance(this);
+        Product produto = new Product(11.50, "Antiferrugem", 3880, 30,10);
+        ProductController productController = new ProductController(connectionSQL);
+
+
+
         Button btn_nova_venda = findViewById(R.id.menu_btn_novaVenda);
         btn_nova_venda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                productController.salvarProdutoController(produto);
+                Log.i("SQLe", "Controller Executado no onclick (MainActivity)");
                 Intent intent = new Intent(MainActivity.this, CheckoutListActivity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/snsolutions/jackout/helper/DBHelper.java
+++ app/src/main/java/com/snsolutions/jackout/helper/DBHelper.java
@@ -8,12 +8,12 @@ public class DBHelper {
     public  ArrayList<Product> productArrayList = new ArrayList<>();
 
     public DBHelper() {
-        this.productArrayList.add(new Product(10.10, "Vassoura", 103, 30));
-        this.productArrayList.add(new Product(10.10, "Vassoura", 103, 30));
-        this.productArrayList.add(new Product(9.95, "Pepino", 104, 30));
-        this.productArrayList.add(new Product(9.95, "Pepino", 104, 30));
-        this.productArrayList.add(new Product(30.31, "Tesoura", 105, 30));
-        this.productArrayList.add(new Product(30.31, "Tesoura", 105, 30));
+        this.productArrayList.add(new Product(10.10, "Vassoura", 103, 30, 10));
+        this.productArrayList.add(new Product(10.10, "Vassoura", 103, 30,10));
+        this.productArrayList.add(new Product(9.95, "Pepino", 104, 30,10));
+        this.productArrayList.add(new Product(9.95, "Pepino", 104, 30,10));
+        this.productArrayList.add(new Product(30.31, "Tesoura", 105, 30,10));
+        this.productArrayList.add(new Product(30.31, "Tesoura", 105, 30,10));
     };
 
     public Product getProductById(int id){
